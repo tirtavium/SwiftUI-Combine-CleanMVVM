5d776f28f61cec11a44093d5082ebfda3ec5b194
tirtagunawan <tirta.gunawan@allianz.com>
2020-07-04 18:05:40 +0700

    troubleshoot action

--- .github/scripts/pipe_test.sh
+++ .github/scripts/pipe_test.sh
@@ -1,4 +1,4 @@
 xcodebuild -project CleanMVVM.xcodeproj \
             -scheme CleanMVVM \
-            -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3' \
+            -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3,id=58B6038B-EA13-47D3-BC28-E163DDD91DCB' \
             clean test
