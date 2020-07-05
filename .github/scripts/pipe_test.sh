b6f1ae3341dffdbb6c1fa728fcf2d0cbd72197e4
tirtagunawan <tirta.gunawan@allianz.com>
2020-07-06 05:54:14 +0700

    troubleshoot action

--- .github/scripts/pipe_test.sh
+++ .github/scripts/pipe_test.sh
@@ -1,3 +1,4 @@
 xcodebuild clean test -project CleanMVVM.xcodeproj \
             -scheme CleanMVVM \
+	    -sdk iphonesimulator \
             -destination "id=211F3388-A5EB-45F1-BE19-3488DDF95C87"
